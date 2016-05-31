0c8f5018be4f42da9004fe357854414bcf90c20b
nazarenolevy <nazareno.levy@despegar.com[merge]>
2016-05-31 11:54:03 -0300

    maps

--- yoga/javascripts/googleMaps.js
+++ yoga/javascripts/googleMaps.js
@@ -1,9 +1,18 @@
 function initialize() {
   var mapProp = {
-    center:new google.maps.LatLng(-34.575754,-58.452212),
+    center: {lat: -34.575754,lng: -58.452212},
     zoom:13,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+  var marker1 = new google.maps.Marker({
+    position: {lat: -34.575754,lng: -58.452212},
+    map: map,
+    title: 'Hello World!'
+  });
+
+  marker1.setMap(map);
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
